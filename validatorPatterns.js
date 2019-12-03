6dba399fe86135309951910e08fc65ffbc7defcd
Chang Cheng <sdc121@CST.SIAST.SK.CA>
2019-12-03 17:29:45 -0600

    add the validators for name, locker number and locker combination

---
+++ validatorPatterns.js
@@ -0,0 +1,18 @@
+"use strict"
+
+jQuery.validator.addMethod("firstName", function(value,elem){
+    return this.optional(elem) || /^[A-Za-z]+[A-Za-z '-]*[A-Za-z]+$/.test(value);
+},"The input first name is invalid");
+
+jQuery.validator.addMethod("lastName", function(value,elem){
+    return this.optional(elem) || /^[A-Za-z]+[A-Za-z '-]*[A-Za-z]+$/.test(value);
+},"The input last name is invalid");
+
+
+jQuery.validator.addMethod("lockerNumber", function(value,elem){
+    return this.optional(elem) || /^1?[0-9]{0, 3}$/.test(value);
+},"The locker number is invalid");
+
+jQuery.validator.addMethod("combination", function(value,elem){
+    return this.optional(elem) || /^[0-5]?\d-[0-5]?\d-[0-5]?\d$/.test(value);
+},"The combination is invalid");
