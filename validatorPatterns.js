2bfe5158e75882fae48bc13acaa1b8b27313ba6d
naishadh26 <58497111+naishadh26@users.noreply.github.com>
2019-12-03 22:53:01 -0600

    Update validatorPatterns.js

--- validatorPatterns.js
+++ validatorPatterns.js
@@ -72,10 +72,100 @@ function insertStudent(students, newStudent){
 
 lockerinfo.html
 -------------------------------------------------------------------------------------------------
-    
+    <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="css/form.css" />
+        <title>Student Locker Information</title>
+        <script src="scripts/lockerInfo.js" defer></script>
+    </head>
+
+    <body>
+        <form id="lockerForm" action="#">
+            <fieldset>
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" />
+
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" />
+
+                <label for="lockerNumber">Locker Number</label>
+                <input type="number" id="lockerNumber" name="lockerNumber" />
+
+                <label for="combination">Locker Combination</label>
+                <input type="text" id="combination" name="combination" />
+                
+                <input type="submit" value="Add" />
+                <input type="reset" value="Clear" />
+            </fieldset>
+        </form>
+        
+        <form id="requestForm" action="#">
+            <fieldset>
+                <label for="student">Select a student:</label>
+                <select id="student">
+                </select>
+
+                <label for="selectedLocker">Locker Number</label>
+                <input type="number" id="selectedLocker" name="lockerNumber" readonly="readonly" />
+
+                <label for="selectedCombination">Combination is:</label>
+                <input type="text" id="selectedCombination"
+                       readonly="readonly" />
+            </fieldset>
+        </form>
+
+        <p>Lockers added:</p>
+        <ol id="lockerList"></ol>
+        <p>No lockers added yet</p>
+    </body>
+</html>
+
     
     
 
     
 lockerinfo.js
 -------------------------------------------------------------------------------------------------    
+"use strict"
+
+let i = 0;
+// let fName = [];
+// let lName = [];
+// let lockName = [];
+// let combinations = [];
+
+let students = [];
+
+window.onload = init;
+function init() {
+    document.getElementById("lockerForm").onsubmit = lockerFormSubmit;
+}
+function lockerFormSubmit(event)
+{
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let lockerNumber = document.getElementById("lockerNumber").value;
+    let combination = document.getElementById("combination").value;
+
+    let student = {"firstName": firstName, "lastName": lastName, "lockerNumber": lockerNumber, "combination": combination};
+    students.push(student);
+
+
+
+    let x = document.getElementById("student");
+    let c = document.createElement("option");
+    c.text = students[i]["lastName"] + ", " + students[i]["firstName"];
+    x.options.add(c, i);
+    document.getElementById("selectedLocker").value = students[i]["lockerNumber"];
+    document.getElementById("selectedCombination").value = students[i]["combination"];
+
+    let node = document.createElement("li");
+    let list = students[i]["lockerNumber"] + ": " + students[i]["lastName"] + ", " + students[i]["firstName"];
+    let textnode = document.createTextNode(list);
+    node.appendChild(textnode);
+    document.getElementById("lockerList").appendChild(node);
+    i++;
+    event.preventDefault();
+}
