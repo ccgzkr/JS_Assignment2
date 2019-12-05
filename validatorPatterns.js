4d3a548b87d2a38bbe6e59e6181bd2d3bd7a458d
naishadh26 <58497111+naishadh26@users.noreply.github.com>
2019-12-04 21:25:49 -0600

    Update validatorPatterns.js

--- validatorPatterns.js
+++ validatorPatterns.js
@@ -74,194 +74,168 @@ lockerinfo.html
 -------------------------------------------------------------------------------------------------
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/form.css" />
+        <title>Student Locker Information</title>
+        <script src="scripts/jquery-3.3.1.min.js"></script>
+        <script src="scripts/jquery.validate.min.js"></script>
+        <script src="scripts/lockerInfo.js" defer></script>
+    </head>
 
-<head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" type="text/css" href="css/form.css" />
-    <title>Student Locker Information</title>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
-    <script src="lockerinfor.js" defer></script>
-</head>
+    <body>
+        <form id="lockerForm" action="#">
+            <fieldset>
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" required pattern="[A-Za-z]+[A-Za-z '-]*[A-Za-z]+"/>
 
-<body>
-<form id="lockerForm" action="#">
-    <fieldset>
-        <label for="firstName">First Name</label>
-        <input type="text" id="firstName" name="firstName" />
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" required pattern="[A-Za-z]+[A-Za-z '-]*[A-Za-z]+"/>
 
-        <label for="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName" />
+                <label for="lockerNumber">Locker Number</label>
+                <input type="number" id="lockerNumber" name="lockerNumber" required pattern="(1[0-9]{3,3}|[1-9]+[0-9]{0,2}|0)"/>
 
-        <label for="lockerNumber">Locker Number</label>
-        <input type="number" id="lockerNumber" name="lockerNumber" />
+                <label for="combination">Locker Combination</label>
+                <input type="text" id="combination" name="combination" required pattern="[0-5]?\d-[0-5]?\d-[0-5]?\d" />
+                
+                <input type="submit" value="Add" id="submit" />
+                <input type="reset" value="Clear" />
+            </fieldset>
+        </form>
+        
+        <form id="requestForm" action="#">
+            <fieldset>
+                <label for="student">Select a student:</label>
+                <select id="student" onclick="setLockerFields()">
+                </select>
 
-        <label for="combination">Locker Combination</label>
-        <input type="text" id="combination" name="combination" />
+                <label for="selectedLocker">Locker Number</label>
+                <input type="number" id="selectedLocker" name="lockerNumber" readonly="readonly" />
 
-        <input type="submit" value="Add" />
-        <input type="reset" value="Clear" />
-    </fieldset>
-</form>
+                <label for="selectedCombination">Combination is:</label>
+                <input type="text" id="selectedCombination"
+                       readonly="readonly" />
+            </fieldset>
+        </form>
 
-<form id="requestForm" action="#">
-    <fieldset>
-        <label for="student">Select a student:</label>
-        <select id="student" onclick="setLockerFields()">
-        </select>
-
-        <label for="selectedLocker">Locker Number</label>
-        <input type="number" id="selectedLocker" name="lockerNumber" readonly="readonly" />
-
-        <label for="selectedCombination">Combination is:</label>
-        <input type="text" id="selectedCombination" readonly="readonly" />
-    </fieldset>
-</form>
-
-<p>Lockers added:</p>
-<ul id="lockerList"></ul>
-<p id="lockerListComment">No lockers added yet</p>
-</body>
-
+        <p>Lockers added:</p>
+        <ul id="lockerList"></ul>
+        <p id="statement">No lockers added yet</p>
+    </body>
 </html>
 
-    
     
 
     
 lockerinfo.js
 -------------------------------------------------------------------------------------------------    
-"use strict";
-
-window.onload = init;
-
-jQuery.validator.addMethod("nameChars", function (value, elem) {
-    ret &= this.optional(elem) || /^[A-Za-z]+[A-Za-z '-]*[A-Za-z]+$/.test(value);
-    return ret;
-}, "The input name is invalid");
-
-jQuery.validator.addMethod("lockerNumberChars", function (value, elem) {
-    ret &= this.optional(elem) || /^1?[0-9]{0, 3}$/.test(value);
-    return ret;}, "The locker number is invalid"
-);
-
-jQuery.validator.addMethod("combinationChars", function(value, elem) {
-        ret &= this.optional(elem) || /^[0-5]?\d-[0-5]?\d-[0-5]?\d$/.test(value);
-        return ret;
-    }, "The combination is invalid"
-);
+"use strict"
 
 let i = 0;
+
 let students = [];
-let ret = true;
 
-const firstNameElement = document.getElementById("firstName");
-const lastNameElement = document.getElementById("lastName");
-const lockerNumberElement = document.getElementById("lockerNumber");
-const combinationElement = document.getElementById("combination");
-
+window.onload = init;
 function init() {
-    document.getElementById("lockerForm").onsubmit = inputValidator;
+    document.getElementById("lockerForm").onsubmit = lockerFormSubmit;
+    document.getElementById("lockerForm").onsubmit = validate;
 }
+function lockerFormSubmit(event)
+{
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let lockerNumber = document.getElementById("lockerNumber").value;
+    let combination = document.getElementById("combination").value;
 
-function inputValidator(event) {
-    event.preventDefault();
-
-    $(document).ready(function(){
-        $("#lockerForm").validate({
-            rules: {
-                firstName: {
-                    required: true,
-                    nameChars: true
-
-                },
-                lastName: {
-                    required: true,
-                    nameChars: true
-
-                },
-                lockerNumber: {
-                    required: true,
-                    lockerNumberChars: true
-
-                },
-                combination: {
-                    required: true,
-                    combinationChars: true
-                },
-            },
-            messages:{
-                firstName:{
-                    required: "Your name?",
-                    nameChars: "Invalid format"
-                },
-                lastName:{
-                    required: "Your name?",
-                    nameChars: "Invalid format"
-                },
-                lockerNumber:{
-                    required: "Your locker number?",
-                    lockerNumberChars: "Invalid format"
-                },
-                combination:{
-                    required: "Your combination?",
-                    combinationChars: "Invalid format"
-                }
-            }
-        })
-    });
-
-    if (ret) {
-        let firstName = firstNameElement.value;
-        let lastName = lastNameElement.value;
-        let lockerNumber = lockerNumberElement.value;
-        let combination = combinationElement.value;
-        lockerFormSubmit(firstName, lastName, lockerNumber, combination);
-    }
-
-    ret = true;
-}
-
-function lockerFormSubmit(firstName, lastName, lockerNumber, combination) {
-    let student = {
-        "firstName": firstName,
-        "lastName": lastName,
-        "lockerNumber": lockerNumber,
-        "combination": combination
-    };
+    let student = {"firstName": firstName, "lastName": lastName, "lockerNumber": lockerNumber, "combination": combination};
     students.push(student);
 
-    let studentRequestItem = document.getElementById("student");
-    let optionItem = document.createElement("option");
-    optionItem.text = students[i]["lastName"] + ", " + students[i]["firstName"];
-    studentRequestItem.options.add(optionItem, i);
+    let selectList = document.getElementById("student");
+    let selectOptionTag = document.createElement("option");
+    selectOptionTag.text = students[i]["lastName"] + ", " + students[i]["firstName"];
+    selectList.options.add(selectOptionTag, i);
     document.getElementById("selectedLocker").value = students[i]["lockerNumber"];
     document.getElementById("selectedCombination").value = students[i]["combination"];
-    studentRequestItem.options[i].selected = true;
+    selectList.options[i].selected = true;
 
     let node = document.createElement("li");
-    let lockerInforList = students[i]["lockerNumber"] + ": " +
-        students[i]["lastName"] + ", " +
-        students[i]["firstName"];
-    let textnode = document.createTextNode(lockerInforList);
+    let list = students[i]["lockerNumber"] + ": " + students[i]["lastName"] + ", " + students[i]["firstName"];
+    let textnode = document.createTextNode(list);
     node.appendChild(textnode);
-    document.getElementById("lockerList").insertBefore(node, document.getElementById("lockerList").firstChild);
-
-    if (student.length !== 0) {
-        document.getElementById("lockerListComment").innerHTML = "";
-    }
-
+    document.getElementById("lockerList").insertBefore(node , document.getElementById("lockerList").firstChild);
+    document.getElementById("statement").innerHTML = "";
     i++;
 
     document.getElementById("firstName").value = "";
     document.getElementById("lastName").value = "";
     document.getElementById("lockerNumber").value = "";
     document.getElementById("combination").value = "";
+    //event.preventDefault();
 }
 
-function setLockerFields() {
+function setLockerFields()
+{
     let index = document.getElementById("student").options.selectedIndex;
     document.getElementById("selectedLocker").value = students[index]["lockerNumber"];
     document.getElementById("selectedCombination").value = students[index]["combination"];
 }
 
+function validate(event) {
+    event.preventDefault();
+    validate1();
+}
+function validate1()
+{
+    let fName1 = document.getElementById("firstName");
+    let lName1 = document.getElementById("lastName");
+    let lockerNo1 = document.getElementById("lockerNumber");
+    let combinationX = document.getElementById("combination");
+
+    $(document).ready(function () {
+        $('#lockerForm').validate({
+            rules: {
+                fName1: {
+                    required: true,
+                    minlength: 2,
+                    pattern: true
+                },
+                lName1: {
+                    required: true,
+                    minlength: 2,
+                    pattern: true
+                },
+                lockerNo1: {
+                    required: true,
+                    min: 1,
+                    max:1999,
+                    pattern: true
+                },
+                combinationX: {
+                    required: true,
+                    pattern: true
+                }
+            },
+
+            message: {
+                fName1: {
+                    required: "Invalid format",
+                    pattern: "Invalid format"
+                },
+                lName1: {
+                    required: "Invalid format",
+                    pattern: "Invalid format"
+                },
+                lockerNo1: {
+                    required: "Invalid format",
+                    pattern: "Invalid format"
+                },
+                combinationX: {
+                    required: "Invalid format",
+                    pattern: "Invalid format"
+                }
+            },
+            submitHandler: lockerFormSubmit,
+
+        });
+    });
+}
